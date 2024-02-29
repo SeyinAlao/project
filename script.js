f3bec59c28ee7421f2f917369d63814e9757987f
SeyinAlao <seyinalao@gmail.com>
2024-02-29 09:50:03 +0100

    Changed the date to Mar 12

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Mar 9, 2024 00:00:00").getTime();
+var countDownDate = new Date("Mar 12, 2024 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
